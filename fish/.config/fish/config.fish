5bdf929b66b31fb2c7cc34160f6ea004cca33918
Rani Idan <25133607+Rani-Idan@users.noreply.github.com>
2017-11-06 10:52:06 +0200

    Added function to attach remote ipython kernel.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,6 +9,7 @@ case Darwin
     set -gx PATH $PATH $HOME/Library/Android/sdk/platform-tools/
     set -gx PATH $PATH $HOME/Library/Android/sdk/build-tools/26.0.1/
     set -gx PATH $PATH $HOME/bin
+    set -gx PATH $PATH $HOME/Library/Python/2.7/bin/
 
     test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
---
+++ fish/.config/fish/functions/ipython_connect.fish
@@ -0,0 +1,3 @@
+function ipython_connect
+	jupyter console --existing $argv
+end
