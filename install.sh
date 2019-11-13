2d2aba39b78366ca07a0c2f537ae40072613300b
Rani <25133607+rani-i@users.noreply.github.com>
2019-11-13 19:18:42 +0200

    Added hybrid vim color theme

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ stow vim
 
 sudo stow --target=/etc/ logid 
 sudo stow --target=/etc/systemd/system/ services
+sudo stow --target=/root/ vim
 
 pushd services/
 for service in *.service; 
@@ -14,3 +15,7 @@ do
     sudo systemctl start $service
 done
 popd
+
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo curl -fLo /root/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
